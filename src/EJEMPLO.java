7a82aee5cc5ed023183476b5ecc9a8bf78c2272c
DAM_1 <DAM_1@PB16>
2019-12-13 19:42:17 +0100

    suma

--- src/EJEMPLO.java
+++ src/EJEMPLO.java
@@ -1,9 +1,17 @@
 
 public class EJEMPLO {
+	
+	public static void suma(){
+		int numero1 = 4;
+		int numero2 = 10;
+		int total = numero1 + numero2;
+		System.out.println(total);
+	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("HOLA");
-
+		suma();
 	}
 
 }
